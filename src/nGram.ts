b2801013df945600f1a6fbc9c54e8d254b603856
Harineko0 <harineko0927@gmail.com>
2021-09-11 22:10:27 +0900

    [Modify] Modify nGram()

--- src/nGram.ts
+++ src/nGram.ts
@@ -1,8 +1,11 @@
 export function nGram(n: number, value: string): string[] {
-    const nGram: string[] = [];
-    const max = value.length - n + 1;
-    for (let index = 0; index < max; index++) {
-        nGram[index] = value.slice(index, index + n)
+    if (n !== 1) {
+        const nGram: string[] = [];
+        const max = value.length - n + 1;
+        for (let index = 0; index < max; index++) {
+            nGram[index] = value.slice(index, index + n)
+        }
+        return nGram;
     }
-    return nGram;
+    return value.split('');
 }
